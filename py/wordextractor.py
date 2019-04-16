2d09363f377265a3a73e195594e7b2d92b5fc8b4
cyclonezhao <lightseeker@qq.com>
2019-04-16 17:18:55 +0800

    将 wordhelper拆成 wordtrainer.py and wordextractor.py

---
+++ py/wordextractor.py
@@ -0,0 +1,17 @@
+def extractWord(sentence):
+    fileExclusive = open(path.sep.join((path.pardir,"resources","extractWords_exclusive")), "rt")
+    wordsExclusive = fileExclusive.read().split("\n")
+    wordsInput = re.compile(r"[\s+-/]").split(sentence)
+
+    reWordPurifier = re.compile(r"[^a-zA-Z']")
+    wordsInput = list(map(lambda v: reWordPurifier.sub("", v).lower(), wordsInput))
+    wordsInput = list(filter(lambda v: len(v.strip())>0 and (v not in wordsExclusive), wordsInput))
+    return wordsInput
+
+wordLst = extractWord(fileInput.read())
+wordGrp = Counter(wordLst)
+sortedByFreq = sorted(wordGrp.items(), key=lambda v: v[1])
+formatted = map(lambda v: "%s : %s" % (v[0], v[1]), sortedByFreq)
+fileOutput = open("%s%swordExtracted" % (path.dirname(fileInput), path.sep), "w")
+fileOutput.write("\n".join(formatted))
+fileOutput.close()
